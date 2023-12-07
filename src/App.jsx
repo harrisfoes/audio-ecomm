27065a0bc6a082c71ae810f7d0af69160f6d99b0
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-12-07 20:34:02 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">
+        Hello world is this a person
+      </p>
+    </div>
+  );
+};
+
+export default App;
