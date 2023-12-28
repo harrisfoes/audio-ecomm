0dfb5e71a7db795776268faea349882e43931a02
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-12-28 20:35:35 +0900

    fix product details

--- src/components/component-pages/ProductPage.jsx
+++ src/components/component-pages/ProductPage.jsx
@@ -10,7 +10,8 @@ function ProductPage() {
   const { productId } = useParams();
   const productDetails = data.filter((items) => {
     return items.slug === productId;
-  });
+  })[0];
+  const pdeets = productDetails;
   console.log(productId);
   console.log(productDetails);
 
@@ -24,25 +25,23 @@ function ProductPage() {
 
           <div className="product-card">
             <div className="pt-4">
-              <img src={baseUrl + productDetails[0].image.mobile.slice(1)} />
+              <img src={baseUrl + productDetails.image.mobile.slice(1)} />
             </div>
             <div>
               <div className="productText py-2">
-                {productDetails[0].new && (
+                {productDetails.new && (
                   <h2 className="py-4 uppercase tracking-[0.5rem] text-creamy-900">
                     New Product
                   </h2>
                 )}
                 <h1 className="w-3/5 text-3xl font-bold uppercase">
-                  {productDetails[0].name}
+                  {productDetails.name}
                 </h1>
-                <p className="pt-4 opacity-50">
-                  {productDetails[0].description}
-                </p>
+                <p className="pt-4 opacity-50">{productDetails.description}</p>
               </div>
               <div className="priceSection py-2">
                 <h2 className="my-4 text-2xl font-bold tracking-widest">
-                  {productDetails[0].price.toLocaleString("en-US", {
+                  {productDetails.price.toLocaleString("en-US", {
                     style: "currency",
                     currency: "USD",
                     minimumFractionDigits: 0,
@@ -67,16 +66,16 @@ function ProductPage() {
           <div className="product-feature py-8">
             <h1 className="text-2xl font-bold">FEATURES</h1>
             <p className="py-4 opacity-50">
-              {productDetails[0].features.split("\n")[0]}
+              {productDetails.features.split("\n")[0]}
             </p>
             <p className="py-4 opacity-50">
-              {productDetails[0].features.split("\n")[2]}
+              {productDetails.features.split("\n")[2]}
             </p>
           </div>
 
           <div className="includes mb-6">
             <h1 className="py-2 text-2xl font-bold">IN THE BOX</h1>
-            {productDetails[0].includes.map((items) => {
+            {productDetails.includes.map((items) => {
               return (
                 <div key={items.item} className="flex gap-8 py-2">
                   <span className="font-bold text-creamy-900">
@@ -91,15 +90,15 @@ function ProductPage() {
           <div className="gallery">
             <img
               className="my-3 rounded-lg"
-              src={baseUrl + productDetails[0].gallery.first.mobile.slice(1)}
+              src={baseUrl + productDetails.gallery.first.mobile.slice(1)}
             />
             <img
               className="my-3 rounded-lg"
-              src={baseUrl + productDetails[0].gallery.second.mobile.slice(1)}
+              src={baseUrl + productDetails.gallery.second.mobile.slice(1)}
             />
             <img
               className="my-3 rounded-lg"
-              src={baseUrl + productDetails[0].gallery.third.mobile.slice(1)}
+              src={baseUrl + productDetails.gallery.third.mobile.slice(1)}
             />
           </div>
 
@@ -108,7 +107,7 @@ function ProductPage() {
               You may also like
             </h1>
             <div>
-              {productDetails[0].others.map((items) => {
+              {productDetails.others.map((items) => {
                 console.log(baseUrl + items.image.mobile.slice(1));
                 return (
                   <div
